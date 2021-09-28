6eb9678893d31bd530104ca8b143b81c232cf4f3
Toon <85719086+Toon-arch@users.noreply.github.com>
2021-09-28 17:46:26 -0400

    Create string_test.lua

---
+++ examples/string_test.lua
@@ -0,0 +1,10 @@
+local LuaH = loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/Toon-arch/LuaH/main/main.lua")))();
+local string = LuaH.string
+
+-- yes, I'm using an unfunny meme for this
+
+local SUS = "AMOGUS"
+SUS = string.lower(SUS) -- amogus
+SUS = string.strip(SUS, "amo") -- gus
+SUS = string.join("|", SUS) -- g|u|s
+print(SUS)
